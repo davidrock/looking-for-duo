03213d09d9275c3bdf6a6411155f3a7e9fd14f14
David Martinelli <david.martinelli@farfetch.com>
2022-09-18 22:22:10 +0100

    Mobile: Add Game screen details

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Image, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Backgrounds } from '../../components/Backgrounds';
 import { useNavigation, useRoute } from '@react-navigation/native';
@@ -10,12 +10,20 @@ import { GameParams } from '../../@types/navigation';
 import { THEME } from '../../theme';
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { Header } from '../../components/Header';
+import { DuoCard, DuoCardProps } from '../../components/DuoCard';
 
 export function Game() {
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
   const route = useRoute();
   const navigation = useNavigation();
   const game = route.params as GameParams;
   console.log(game);
+
+  useEffect(() => {
+    fetch(`http://192.168.1.66:3333/games/${game.id}/ads`)
+      .then((response) => response.json())
+      .then((data) => setDuos(data));
+  }, []);
 
   function handleGoBack() {
     navigation.goBack();
@@ -47,6 +55,25 @@ export function Game() {
         <Header
           title={game.title}
           subtitle='Conecte-se e comece a jogar!'></Header>
+
+        <FlatList
+          data={duos}
+          horizontal
+          contentContainerStyle={[
+            duos.length > 0 ? styles.contentList : styles.emptyListContent,
+          ]}
+          style={styles.containerList}
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => item.id}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anúncios publicados para esse jogo ainda
+            </Text>
+          )}
+          renderItem={({ item }) => (
+            <DuoCard data={item} onConnect={() => {}} />
+          )}
+        />
       </SafeAreaView>
     </Backgrounds>
   );
